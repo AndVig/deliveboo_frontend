b65cb1694e90f31cf6e7b5494e146de5fce64a56
MicheleRaisa95 <micheleraisa95@gmail.com>
2024-09-12 15:48:11 +0200

    UPD aggiunto style.scss a appheader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,6 @@
 <script>
 export default {
-  name: 'AppHeader',
+  name: "AppHeader",
 };
 </script>
 
@@ -8,16 +8,28 @@ export default {
   <header class="py-3 mb-3 border-bottom">
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
-        
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <router-link :to="{ name: 'home' }" class="nav-link active"  aria-current="page"> <strong>Deliveboo</strong> </router-link>
-            </li> 
+              <router-link
+                :to="{ name: 'home' }"
+                class="nav-link active"
+                aria-current="page"
+              >
+                <strong>Deliveboo</strong>
+              </router-link>
+            </li>
           </ul>
           <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button class="btn btn-outline-success" type="submit">
+              Search
+            </button>
           </form>
         </div>
       </div>
@@ -26,13 +38,13 @@ export default {
 </template>
 
 <style scoped lang="scss">
-@use '../scss/partials/variables' as *;
+// @use "../scss/partials/variables" as *;
 header {
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
-  color:$main-color;
+  background-color: $main-color;
   /* h1 {
     color: red;
   } */
