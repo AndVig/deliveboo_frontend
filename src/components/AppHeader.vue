35d6667ab8fd554d69adc8715c81bfef53a47903
AndVig <andrea.vigna2@gmail.com>
2024-09-12 13:37:27 +0200

    UPD Header style

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,42 +8,33 @@ export default {
   <header class="py-3 mb-3 border-bottom">
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
-        <a class="navbar-brand"><strong>Delivebool</strong></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+        
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <router-link :to="{ name: 'home' }" class="nav-link active" aria-current="page">Home</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link :to="{ name: 'menu' }" class="nav-link active" aria-current="page">Menù</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link :to="{ name: 'carrello' }" class="nav-link active" aria-current="page">Carrello</router-link>
-            </li>
+              <router-link :to="{ name: 'home' }" class="nav-link active"  aria-current="page"> <strong>Deliveboo</strong> </router-link>
+            </li> 
           </ul>
-          <!-- <form class="d-flex" role="search">
+          <form class="d-flex" role="search">
             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success" type="submit">Search</button>
-          </form> -->
+          </form>
         </div>
       </div>
     </nav>
   </header>
 </template>
 
-<style>
+<style scoped lang="scss">
+@use '../scss/partials/variables' as *;
 header {
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
-
-  h1 {
+  color:$main-color;
+  /* h1 {
     color: red;
-  }
+  } */
 }
 </style>
