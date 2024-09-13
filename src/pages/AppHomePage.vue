2dc5f0cb5b4b502a8231633c38f6a7fcf0c4e80a
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-09-13 10:57:47 +0200

    UPD chiamata api ristoranti

--- src/pages/AppHomePage.vue
+++ src/pages/AppHomePage.vue
@@ -1,11 +1,49 @@
 <script>
+import axios from "axios";
 export default {
   name: 'AppHomePage',
+  data() {
+    return {
+      api: {
+        baseUrl: 'http://127.0.0.1:8000',
+        endPoints: {
+          restaurantsList: '/api/restaurants'
+        }
+      },
+      response: {},
+    }
+  },
+  methods: {
+    getRestaurants() {
+      const apiUrl = this.api.baseUrl + this.api.endPoints.restaurantsList;
+      axios.get(apiUrl)
+        .then((response) => {
+          console.log(response.data);
+          this.response = response.data;
+        })
+        .catch((error) => {
+          console.error('Errore nel recupero dei ristoranti:', error);
+        });
+    }
+  },
+  created() {
+    this.getRestaurants();
+
+  }
 };
 </script>
 
 <template>
   <h1>Home</h1>
+  <div>
+    <div class="d-flex">
+      <div class="row">
+        <div class="col-4 py-3" v-for="restaurant in response.data" :key="restaurant.id">
+          <h1 class="py-4">{{ restaurant.companyName }}</h1>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style></style>
--- src/pages/AppMenuPage.vue
+++ src/pages/AppMenuPage.vue
@@ -6,6 +6,17 @@ export default {
 
 <template>
     <h1>menu</h1>
+    <div>
+        <div class="row">
+            <div class="col-4 py-3" v-for="restaurants in response.results?.data">
+                <ListProject :title="project.title" :slug="project.slug" :description="project.description" />
+            </div>
+            <nav class="d-flex justify-content-around">
+                <div class="btn btn-primary" @click="prevPage" v-if="response.results?.prev_page_url">Prev</div>
+                <div class="btn btn-primary" @click="nextPage" v-if="response.results?.next_page_url">Next</div>
+            </nav>
+        </div>
+    </div>
 </template>
 
 <style></style>
