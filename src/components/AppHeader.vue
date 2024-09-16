29d9274dcc3e8a29fef855bb552d05c7d6167c1e
AndVig <andrea.vigna2@gmail.com>
2024-09-16 18:19:32 +0200

    FIX link al backoffice

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -11,7 +11,7 @@ export default {
         <router-link :to="{ name: 'home' }" class="nav-link active navbar-brand" aria-current="page"><img src="/img/logo.png" alt="Deliveroo logo" height="100"></router-link>
         
         <div class="navbar-nav ms-auto">
-          <a href="#" class="nav-item nav-link text-white d-flex align-items-center me-3">
+          <a href="http://127.0.0.1:8000/login" class="nav-item nav-link text-white d-flex align-items-center me-3">
             <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" class="me-1">
               <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
               <polyline points="9 22 9 12 15 12 15 22"></polyline>
